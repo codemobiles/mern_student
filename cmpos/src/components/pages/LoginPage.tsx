4882decd82991a54b08a5ddb45d7150326fbac9c
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-03 15:07:19 +0700

    1234

--- cmpos/src/components/pages/LoginPage.tsx
+++ cmpos/src/components/pages/LoginPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Box, Button, Card, TextField, Typography } from "@mui/material";
 
-type Props = {};
-
-export default function LoginPage({}: Props) {
-  return <div>LoginPage</div>;
+export default function LoginPage() {
+  return (
+    <Box>
+      <Card elevation={10} className="p-5">
+        <Typography variant="h4" className="!mb-3">
+          Login
+        </Typography>
+        <form>
+          <TextField fullWidth label="Username" />
+          <div className="h-2"></div>
+          <TextField fullWidth label="Password" />
+          <div className="h-2"></div>
+          <Button fullWidth variant="contained">
+            Submit
+          </Button>
+        </form>
+      </Card>
+    </Box>
+  );
 }
