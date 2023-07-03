8f896eecd7dc65d78a0c109a9309292763eec238
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-03 13:23:32 +0700

    1234

--- cmpos/src/DemoButtons.tsx
+++ cmpos/src/DemoButtons.tsx
@@ -1,12 +1,17 @@
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
 
-export default function BasicButtons() {
+export default function IconLabelButtons() {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <Stack direction="row" spacing={2}>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
     </Stack>
   );
 }
