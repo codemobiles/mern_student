06cd2fee4adfe5968fd45cf5931e6cf3f6e66423
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-04 13:15:24 +0700

    1234

--- demoPOS/backend/src/routes.ts
+++ demoPOS/backend/src/routes.ts
@@ -3,6 +3,19 @@ import { UserController } from "./controller/UserController";
 
 export const Routes = [
   {
+    method: "post",
+    route: "/register",
+    controller: UserController,
+    action: "register",
+  },
+  {
+    method: "post",
+    route: "/login",
+    controller: UserController,
+    action: "login",
+  },
+
+  {
     method: "get",
     route: "/accounts",
     controller: UserController,
