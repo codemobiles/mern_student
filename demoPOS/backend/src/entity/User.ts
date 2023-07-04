e046c0c276dc966c0040f1e9cb30ab4672f66ee0
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-04 11:21:29 +0700

    1234

--- demoPOS/backend/src/entity/User.ts
+++ demoPOS/backend/src/entity/User.ts
@@ -1,18 +1,23 @@
-import { Entity, ObjectIdColumn, ObjectId, Column } from "typeorm"
+import { Entity, ObjectIdColumn, ObjectId, Column } from "typeorm";
 
 @Entity()
 export class User {
+  @ObjectIdColumn()
+  id: ObjectId;
 
-    @ObjectIdColumn()
-    id: ObjectId
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
-
+  @Column()
+  age: number;
 }
+
+// "_id": "5aa7c5137a3066a2b958947d",
+// "username": "chaiyasit.t@gmail.com",
+// "password": "$2a$08$mq4EZf1kuYoLQ2pgwFErTu0ULXnfSWrIimnZzCBS6XUPlWHfjksl6",
+// "level": "normal",
+// "created": "2018-03-13T12:33:23.161Z",
+// "__v": 0
