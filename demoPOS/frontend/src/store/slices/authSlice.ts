04fb27309a3a5273b08f7a44f03d69fff9bdb9ec
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-04 15:11:18 +0700

    1234

--- demoPOS/frontend/src/store/slices/authSlice.ts
+++ demoPOS/frontend/src/store/slices/authSlice.ts
@@ -1,8 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+type AuthState = {
+  count: number;
+};
+
+const initialState: AuthState = {
+  count: 0,
+};
+
 const authSlice = createSlice({
   name: "auth",
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
