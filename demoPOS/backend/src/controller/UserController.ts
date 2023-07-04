0deeada8e8b8c166e79839d3d17e7548d9da0a7a
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-04 13:38:32 +0700

    1234

--- demoPOS/backend/src/controller/UserController.ts
+++ demoPOS/backend/src/controller/UserController.ts
@@ -9,19 +9,17 @@ export class UserController {
   private userRepository = AppDataSource.getMongoRepository(Users);
 
   async register(req: Request, res: Response, next: NextFunction) {
-    // try {
-    //   req.body.created = savedValue(req.body.created, new Date());
-    //   req.body.level = savedValue(req.body.level, "normal");
-    //   req.body.__v = savedValue(req.body.__v, 0);
+    try {
+      req.body.created = savedValue(req.body.created, new Date());
+      req.body.level = savedValue(req.body.level, "normal");
+      req.body.__v = savedValue(req.body.__v, 0);
 
-    //   req.body.password = await bcrypt.hash(req.body.password, 8);
-    //   const doc = await this.userRepository.save(req.body);
-    //   return { result: "ok", message: doc };
-    // } catch (e) {
-    //   return { result: "nok", message: "invalid data" };
-    // }
-
-    return { result: "ok" };
+      req.body.password = await bcrypt.hash(req.body.password, 8);
+      const doc = await this.userRepository.save(req.body);
+      return { result: "ok", message: doc };
+    } catch (e) {
+      return { result: "nok", message: JSON.stringify(e) };
+    }
   }
 
   async all(req: Request, res: Response, next: NextFunction) {
