f2527fd94ea3157ae3080b2fcf6e2ba7cd35bffd
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 10:54:34 +0700

    1234

--- demoPOS/frontend/src/store/slices/stockSlice.ts
+++ demoPOS/frontend/src/store/slices/stockSlice.ts
@@ -1,8 +1,19 @@
+import { Product } from "@/types/product.type";
 import { createSlice } from "@reduxjs/toolkit";
+
+export interface StockState {
+  stockAllResult: Product[];
+  stockOneResult: Product | null;
+}
+
+const initialState: StockState = {
+  stockAllResult: [],
+  stockOneResult: null,
+};
 
 const stockSlice = createSlice({
   name: "stock",
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
