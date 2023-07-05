e2c8473cc6d87bac404dc9b5793cb7291c03d9ac
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 11:14:15 +0700

    1234

--- demoPOS/frontend/src/components/pages/StockPage.tsx
+++ demoPOS/frontend/src/components/pages/StockPage.tsx
@@ -1,24 +1,39 @@
-import { getProducts, stockSelector } from "@/store/slices/stockSlice";
+import * as React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { stockSelector, getProducts } from "@/store/slices/stockSlice";
 import { useAppDispatch } from "@/store/store";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
+const columns: GridColDef[] = [
+  { field: "product_id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Name", width: 330 },
+  { field: "stock", headerName: "Stock", width: 130 },
+  { field: "price", headerName: "Price", width: 130 },
+  { field: "created", headerName: "Created", width: 130 },
+];
 
 export default function StockPage() {
   const dispatch = useAppDispatch();
   const stockReducer = useSelector(stockSelector);
 
-  useEffect(() => {
+  React.useEffect(() => {
     dispatch(getProducts(""));
   }, [dispatch]);
 
   return (
-    <div>
-      <h2>Stock</h2>
-      <ul>
-        {stockReducer.stockAllResult.map((item) => (
-          <li key={item._id}>{item.name}</li>
-        ))}
-      </ul>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        getRowId={(row) => row._id}
+        rows={stockReducer.stockAllResult}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
     </div>
   );
 }
