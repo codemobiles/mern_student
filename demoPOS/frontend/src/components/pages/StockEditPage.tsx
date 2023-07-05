07601808f58e60f00ed9dc8d3e279d6f102216ca
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 15:21:10 +0700

    1234

--- demoPOS/frontend/src/components/pages/StockEditPage.tsx
+++ demoPOS/frontend/src/components/pages/StockEditPage.tsx
@@ -181,14 +181,12 @@ const StockEdit = () => {
       <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
         <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">Are you sure to exit?</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
-            Agree
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => navigate("/stock")} autoFocus>
+            Sure
           </Button>
         </DialogActions>
       </Dialog>
