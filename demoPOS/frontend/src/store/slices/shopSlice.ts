d9baaa6e251a1eafa9dbd092d8d24e4b47f440fc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 15:45:58 +0700

    1234

--- demoPOS/frontend/src/store/slices/shopSlice.ts
+++ demoPOS/frontend/src/store/slices/shopSlice.ts
@@ -1,7 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { Product } from "@/types/product.type";
+import { TransactionResponse } from "@/types/transaction.type";
+import { server } from "@/utils/constants";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { httpClient } from "@/utils/HttpClient";
 
-const shopSlice = createSlice({ name: "shop", initialState: {}, reducers: {} });
+export interface ShopState {
+  transactionAllResult: TransactionResponse[];
+  mOrderLines: Product[];
+  mTotalPrice: number;
+  mTaxAmt: number;
+  mIsPaymentMade: boolean;
+  mGiven: number;
+}
+
+const initialState: ShopState = {
+  transactionAllResult: [],
+  mOrderLines: [],
+  mTotalPrice: 0,
+  mTaxAmt: 0,
+  mIsPaymentMade: false,
+  mGiven: 0,
+};
+
+const shopSlice = createSlice({ name: "shop", initialState, reducers: {} });
 
 export default shopSlice.reducer;
 export const shopSelector = (state: RootState) => state.shopReducer;
