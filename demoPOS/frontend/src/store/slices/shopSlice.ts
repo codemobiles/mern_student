9cbd92c24f812566c620dfdfb77ff21256bb0590
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 15:46:40 +0700

    1234

--- demoPOS/frontend/src/store/slices/shopSlice.ts
+++ demoPOS/frontend/src/store/slices/shopSlice.ts
@@ -23,6 +23,28 @@ const initialState: ShopState = {
   mGiven: 0,
 };
 
+export const getTransactions = createAsyncThunk("shop/transaction", async (): Promise<TransactionResponse[]> => {
+  const result = await httpClient.get<TransactionResponse[]>(server.TRANSACTION_URL);
+  return result.data;
+});
+
+const updateOrder = (state: ShopState, orderLines: any) => {
+  let totalPrice = 0;
+  let taxAmt = 0;
+  for (const item of orderLines) {
+    totalPrice += item.price * item.qty;
+  }
+  taxAmt = totalPrice * 0.07;
+  state.mOrderLines = orderLines;
+  state.mTotalPrice = totalPrice;
+  state.mTaxAmt = taxAmt;
+};
+
+export const submitPayment = createAsyncThunk("shop/submit", async (data: any) => {
+  const result = await httpClient.post(server.TRANSACTION_URL, data);
+  return result.data;
+});
+
 const shopSlice = createSlice({ name: "shop", initialState, reducers: {} });
 
 export default shopSlice.reducer;
