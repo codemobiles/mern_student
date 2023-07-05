fff442073107a9ee56b823f817ca7c1a74d5a0df
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 13:43:50 +0700

    1234

--- demoPOS/frontend/src/components/pages/StockCreatePage.tsx
+++ demoPOS/frontend/src/components/pages/StockCreatePage.tsx
@@ -12,6 +12,7 @@ import { Product } from "@/types/product.type";
 import { useForm, Controller } from "react-hook-form";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { CropDin } from "@mui/icons-material";
 
 const formValidateSchema = Yup.object().shape({
   name: Yup.string().required("Name is required").trim(),
@@ -40,6 +41,8 @@ export default function StockCreatePage() {
             <Typography gutterBottom variant="h3">
               Create Product
             </Typography>
+            {/* Name */}
+            <Controller control={control} name="name" render={({ field }) => <TextField label="Name" {...field} fullWidth />} />
           </CardContent>
         </Card>
       </form>
