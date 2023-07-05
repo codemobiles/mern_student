98ae31d26cfb1cd4b105656db03c60289e1bba0b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 10:51:54 +0700

    1234

---
+++ demoPOS/frontend/src/store/slices/stockSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const stockSlice = createSlice({
+  name: "stock",
+  initialState: {},
+  reducers: {},
+});
+
+export default stockSlice.reducer;
