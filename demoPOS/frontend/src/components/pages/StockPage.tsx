e9cc1f5f310a40c2fc38bc2698458771bb04b6dc
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-05 09:15:49 +0700

    1234

---
+++ demoPOS/frontend/src/components/pages/StockPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function StockPage({}: Props) {
+  return (
+    <div>StockPage</div>
+  )
+}
---
+++ demoPOS/frontend/src/components/pages/TransactionPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function TransactionPage({}: Props) {
+  return <div>TransactionPage</div>;
+}
