8e47c80419a469d28814373ae0364988a2f4d5a9
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-06 13:19:10 +0700

    1234

--- demoPOS/frontend/src/components/pages/TransactionPage.tsx
+++ demoPOS/frontend/src/components/pages/TransactionPage.tsx
@@ -2,6 +2,15 @@ import { getTransactions, shopSelector } from "@/store/slices/shopSlice";
 import { useAppDispatch } from "@/store/store";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "transaction_id", headerName: "ID", width: 70 },
+  { field: "total", headerName: "Total", width: 130 },
+  { field: "paid", headerName: "Paid", width: 130 },
+  { field: "change", headerName: "Change", width: 130 },
+  { field: "timestamp", headerName: "Timestamp", width: 130 },
+];
 
 export default function TransactionPage() {
   const shopReducer = useSelector(shopSelector);
@@ -11,5 +20,20 @@ export default function TransactionPage() {
     dispatch(getTransactions());
   }, [dispatch]);
 
-  return <div>TransactionPage {JSON.stringify(shopReducer.transactionAllResult)}</div>;
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        getRowId={(row) => row.transaction_id}
+        rows={shopReducer.transactionAllResult}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
 }
