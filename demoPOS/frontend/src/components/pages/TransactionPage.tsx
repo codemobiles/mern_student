d8827186cfdfd66abbd8bcbf94019057bc0772c3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-06 11:55:54 +0700

    1234

--- demoPOS/frontend/src/components/pages/TransactionPage.tsx
+++ demoPOS/frontend/src/components/pages/TransactionPage.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 type Props = {};
 
 export default function TransactionPage({}: Props) {
-  return <div>TransactionPage</div>;
+
+  // useEffect
+  // getTransactions
+
+  return <div>TransactionPage {JSON.stringify(transactionAllResult)}</div>;
 }
