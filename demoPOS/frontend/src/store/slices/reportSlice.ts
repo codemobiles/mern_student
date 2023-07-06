d1af1de895a8edc0d06b9ec32e20eb4544a5f334
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-06 14:16:19 +0700

    1234

--- demoPOS/frontend/src/store/slices/reportSlice.ts
+++ demoPOS/frontend/src/store/slices/reportSlice.ts
@@ -1,9 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
+import { ChartType } from "chart.js";
+
+// const [chartType, setChartType] = useState<ChartType>("line");
+// const [chartData1, setChartData1] = useState([]);
+// const [chartData2, setChartData2] = useState([]);
+
+type StateProps = {
+  chartType: ChartType;
+  chartData1: any[];
+  chartData2: any[];
+};
+
+const initialState: StateProps = {
+  chartType: "line",
+  chartData1: [],
+  chartData2: [],
+};
 
 const reportSlice = createSlice({
   name: "report",
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
