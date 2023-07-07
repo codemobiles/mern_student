3e19fb50cf79563066fed8862ea18f37e40bbde7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-07-07 10:41:12 +0700

    1234

--- demoPOS/backend/src/data-source.ts
+++ demoPOS/backend/src/data-source.ts
@@ -8,8 +8,10 @@ import { Counters } from "./entity/Counters";
 export const AppDataSource = new DataSource({
   type: "mongodb",
   database: "demopos",
-  port: 27018,
-  host: "127.0.0.1",
+  // port: 27018,
+  // host: "127.0.0.1",
+  host: process.env.MONGO_HOST ? process.env.MONGO_HOST : "localhost",
+  port: process.env.MONGO_PORT ? Number(process.env.MONGO_PORT) : 27018,
   synchronize: true,
   logging: false,
   entities: [Users, Products, Transactions, Counters],
